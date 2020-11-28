01623e9ac5d30265a59de42e68720a5645a80a22
Casiano Fernandez <casiano_fernandez@yahoo.com>
2020-11-28 20:07:12 +0800

    fix Link

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -10,11 +10,12 @@ export default function Dashboard() {
                This website is under development.
             </div>
             <div className="lead text-justify col-sm-10 mx-auto mb-5">
-               In this website you can create a blog post, in the <Link to="/">Home</Link>{" "}
-               page. Existing blog post by all the users are provided below and below the
-               home page. You can also view the detail of the post by following the "View"
-               link. Also, you can preform delete if you are the author of the post. You
-               are not allowed to access some page if you are not authenticate.
+               In this website you can create a blog post, in the{" "}
+               <Link to="/posts">Home</Link> page. Existing blog post by all the users are
+               provided below and below the home page. You can also view the detail of the
+               post by following the "View" link. Also, you can preform delete if you are
+               the author of the post. You are not allowed to access some page if you are
+               not authenticate.
                <p className="d-flex justify-content-end">- casiano_fernandez@gmail.com</p>
             </div>
          </Jumbotron>
